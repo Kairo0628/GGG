b509a7d5c13cdb6ee415bf2e1d5cc5b73dfd2d1b
Kairo0628 <kairo_o@naver.com>
2023-03-14 16:52:50 +0900

    202303141652

--- Python/OneCard.py
+++ Python/OneCard.py
@@ -1,18 +1,19 @@
-# 하나의 클래스, 사람 대 컴퓨터, 트럼프 카드, 공격, 수비, 카드 뽑기, 카드 다시 섞기, 조커 카드
+# 파이썬 원카드
+
+# 공격, 수비, 조커, 등등
 
 import random
 
 class OneCard:
   def __init__(self):
-    self.CreateGame()
-
+    self.Create_game()
+    
     print("게임 시작 !")
     print("\n")
-    
-    while self.End():
-      self.Every_turn()
 
-  def CreateGame(self):
+    self.Every_turn()
+
+  def Create_game(self):
     self.deck = []
     
     for i in range(1, 14):
@@ -48,55 +49,66 @@ class OneCard:
   def Show_field(self):
     print(f"현재 필드 : {self.field[-1]}")
 
-  def End(self):
-    if len(self.cHand) == 0:
-      print("컴퓨터의 승리 ㅜ_ㅜ")
-      return False
-    elif len(self.pHand) == 0:
-      print("당신의 승리!")
-      return False
-    else:
-      return True
-
   def Can_play(self, hand):
     can_play = []
     for i in hand:
-      if self.field[-1][0] == i[0]:
+      if i[0] == self.field[-1][0]:
         can_play.append(i)
-      elif self.field[-1][1] == i[1]:
+      elif i[1] == self.field[-1][1]:
         can_play.append(i)
-    
+
     return can_play
-  
+
   def Every_turn(self):
-    print("컴퓨터의 차례")
-    self.Show_field()
+    while True:
+      try:
+        print("컴퓨터의 차례입니다.")
+        self.Show_field()
+        cPlay = self.Can_play(self.cHand)
+        if len(cPlay) == 0:
+          print("컴퓨터가 낼 카드가 없어 한장 뽑습니다.")
+          self.DrawC()
+        else:
+          select = random.randrange(1, len(cPlay) + 1)
+          selected = cPlay[select - 1]
+          self.cHand.remove(selected)
+          self.field.append(selected)
+          print(f"컴퓨터가 {selected}를 냈습니다.")
+        print(f"남은 컴퓨터의 카드 수 : {len(self.cHand)}")
+        print("\n")
 
-    cPlay = self.Can_play(self.cHand)
-    if len(cPlay) == 0:
-      self.cHand.append(self.deck.pop())
-      print("컴퓨터가 낼 카드가 없어 한장 뽑습니다.")
-    else:
-      select = random.randint(0, len(cPlay))
-      print(f"컴퓨터가 {self.cHand[select]}를 냈습니다.")
-      self.field.append(self.cHand.pop(select))
-    print(f"남은 컴퓨터의 카드 : {len(self.cHand)}")
-    print("\n")
+        if len(self.cHand) == 0:
+          print("컴퓨터의 승리! ㅜ_ㅜ")
+          break
 
-    print("당신의 차례")
-    self.Show_field()
-    print(f"당신의 패 : {self.pHand}")
+        print("플레이어의 차례입니다.")
+        self.Show_field()
+        print(f"당신의 패 : {self.pHand}")
+        pPlay = self.Can_play(self.pHand)
+        if len(pPlay) == 0:
+          print("플레이어가 낼 카드가 없어 한장 뽑습니다.")
+          self.DrawP()
+        else:
+          print(f"낼 수 있는 카드 : {pPlay}")
+          select = int(input("낼 카드를 선택하세요 : "))
+          selected = pPlay[select - 1]
+          self.pHand.remove(selected)
+          self.field.append(selected)
+          print(f"플레이어가 {selected}를 냈습니다.")
+        print(f"남은 플레이어의 카드 수 : {len(self.pHand)}")
+        print("\n")
 
-    pPlay = self.Can_play(self.pHand)
-    if len(pPlay) == 0:
-      self.pHand.append(self.deck.pop())
-      print("낼 카드가 없어 한장 뽑습니다.")
-    else:
-      print(f"낼 수 있는 카드 : {pPlay}")
-      select = int(input("낼 카드를 고르시오 : "))
-      print(f"{self.pHand[select - 1]}을 냈습니다.")
-      self.field.append(self.pHand.pop(select - 1))
-    print(f"당신의 남은 카드 : {len(self.pHand)}")
-    print("\n")
- 
+        if len(self.pHand) == 0:
+          print("플레이어의 승리 !")
+          break
+          
+      except:
+        print("\n")
+        print("덱을 섞는 중...")
+        print("\n")
+        temp = self.field.pop()
+        self.deck = self.field
+        random.shuffle(self.deck)
+        self.field.append(temp)
+
 OneCard()
